dcbea0a6a4f67f680c197b1ab117dd34494c1be8
Gabrielgsw <ggermanow279@gmail.com>
2023-05-08 17:38:58 -0300

    primeiro commit

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Exo.WebApi.Models;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext(){}
+
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)  
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                // String de conexao própria
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;"+"Database=ExoApi;Trusted_Connection = True;");
+                // Ex 1 de string de conexao: 
+                // User ID=sa;Password=admin;Server=localhost;Database=ExoApi; +Trusted_Connection=False;
+
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set;}
+    }
+
+}
